86da1b4e8b956e1a63ed6aedaf883f629b80663a
Sebastian Castro <sebascastrom95@gmail.com>
2022-04-28 22:55:33 -0500

    Changing var for let and const and updating to camel case

--- JS/randomNumbers/naleatorio.js
+++ JS/randomNumbers/naleatorio.js
@@ -1,22 +1,18 @@
-var z, i;
-var reps1 = document.getElementById("nnumeros");
-var mini1 = document.getElementById("nmin");
-var maxi1 = document.getElementById("nmax");
-var resultados = document.getElementById("resultados");
-
+let z, i;
+let reps1 = document.getElementById("nNumeros");
+let mini1 = document.getElementById("nMin");
+let maxi1 = document.getElementById("nMax");
+let resultados = document.getElementById("resultados");
 // var resultadox = document.getElementById("nresultado");
-
-var button = document.getElementById("boton");
-
+const button = document.getElementById("boton");
 button.addEventListener("click", generar)
-
 function generar() {
-  var reps = parseInt(reps1.value);
-  var mini = parseInt(mini1.value);
-  var maxi = parseInt(maxi1.value);
+  let reps = parseInt(reps1.value);
+  let mini = parseInt(mini1.value);
+  let maxi = parseInt(maxi1.value);
 
-  for (var i = 0; i < reps; i++) {
-    var z = aleatorio(mini, maxi);
+  for (let i = 0; i < reps; i++) {
+    let z = aleatorio(mini, maxi);
     if (i == reps-1){
       resultados.innerHTML += `${z} </br> `
     }
@@ -25,9 +21,8 @@ function generar() {
     }
   }
 }
-
 function aleatorio(min, max) {
-  var resultado;
+  let resultado;
   resultado = Math.floor(Math.random() * (max - min + 1)) + min;
   return resultado;
 }
