a1a6db0468e08a6b3f44fc9f5471cc846501bd13
Sebastian Castro <sebascastrom95@gmail.com>
2023-01-16 23:57:07 -0500

    Update JS-CSS include random PC

--- JS/rockPapperScissors/piedraPapelTijera.js
+++ JS/rockPapperScissors/piedraPapelTijera.js
@@ -1,23 +1,33 @@
 // 1 es piedra, 2 es papel y 3 es tijera
 let nombre = prompt ("Nombre del jugador:")
+let board = document.getElementById("dashboard")
 const jugar = document.getElementById("boton");
 let opcionJugador1 = document.getElementById("opcionJugador")
-let PC = 2
+let PC = 0
 let rJugador = 0
 let rPC = 0
-
 jugar.addEventListener("click", resultado);
+
 function resultado() {
+    PC = Math.floor(Math.random()*(3 - 1 + 1))+1
+
+    if (PC == 1){
+        alert ('PC saco Piedra')
+    } else if (PC == 2){
+        alert ('PC saco Papel')
+    } else if (PC == 3){
+        alert ('PC saco Tijera')
+    }
     if (opcionJugador1.value == "") {
         alert('INGRESE UNA OPCION')
     } else if (opcionJugador1.value == PC) {
         alert('EMPATE')
     } else if((opcionJugador1.value == 1 && PC == 3) || (opcionJugador1.value == 2 && PC == 1) || (opcionJugador1.value == 3 && PC == 2)) {
         alert('GANASTE')
-        rJugador = rJugador + 1
+        rJugador += 1
     } else {
         alert('PERDISTE')
-        rPC = rPC + 1
+        rPC += 1 
     }
-    alert (nombre + ": " + rJugador + " - PC: " + rPC)
+    board.innerHTML = nombre + ": " + rJugador + " - PC: " + rPC
 }
